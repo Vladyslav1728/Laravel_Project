dfa195bf5d70c73bbe078c863259f7db923577c8
Vladyslav Shcherbyna <158246943+Vladyslav1728@users.noreply.github.com>
2026-03-02 10:17:49 +0100

    lesson - Rest API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookRpcController;
+use App\Http\Controllers\BookRestController;
+use App\Http\Controllers\BookSacController;
+use App\Http\Controllers\BookApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/rpc/books/{id}/borrow', [BookRpcController::class, 'borrowBook']);
+Route::post('/rpc/books/{id}/return', [BookRpcController::class, 'returnBook']);
+
+Route::get('/sac/books/{id}', BookSacController::class);
+
+Route::prefix('rest')->group(function () {
+    Route::resource('books', BookRestController::class);
+});
+
+Route::prefix('restapi')->group(function () {
+    Route::apiresource('books', BookApiController::class);
+});
